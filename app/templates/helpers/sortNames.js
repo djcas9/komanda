5b76858f246ff0f29ce69c377e78e015021095b6
Eugene Bulkin <ebulkin@caltech.edu>
2014-06-19 16:50:12 -0700

    Add icons for other user modes (&, ~, %)

--- app/templates/helpers/sortNames.js
+++ app/templates/helpers/sortNames.js
@@ -5,59 +5,40 @@ define('templates/helpers/sortNames', [
 
   Handlebars.registerHelper('sortNames', function(context, options) {
     var buffer = "";
-    var op = [];
-    var voice = [];
-    var normal = [];
+    var symbols = ["&", "~", "@", "%", "+", ""];
+    var users = {
+      "&": [],
+      "~": [],
+      "@": [],
+      "%": [],
+      "+": [],
+      "": []
+    };
+
+    var sortFn = function (a, b) {
+      return a[0].toLowerCase().localeCompare(b[0].toLowerCase());
+    };
 
     for (var user in context) {
       var value = context[user];
+      users[value].push([user, value]);
+    }
 
-      if (value === "@") {
-        op.push(user);
-      } else if (value === "+") {
-        voice.push(user);
-      } else {
-        normal.push(user);
+    for (var key in users) {
+      if (users.hasOwnProperty(key)) {
+        users[key].sort(sortFn);
       }
     }
 
-    op.sort(function (a, b) {
-      return a.toLowerCase().localeCompare(b.toLowerCase());
-    });
-
-    voice.sort(function (a, b) {
-      return a.toLowerCase().localeCompare(b.toLowerCase());
-    });
-
-    normal.sort(function (a, b) {
-      return a.toLowerCase().localeCompare(b.toLowerCase());
-    });
-
-    for (var i = 0; i < op.length; i += 1) {
-      var name = op[i];
-
-      buffer += options.fn({
-        name: name,
-        value: "@"
-      });
-    }
-
-    for (var x = 0; x < voice.length; x += 1) {
-      var xname = voice[x];
-
-      buffer += options.fn({
-        name: xname,
-        value: "+"
-      });
-    }
-
-    for (var y = 0; y < normal.length; y += 1) {
-      var yname = normal[y];
-
-      buffer += options.fn({
-        name: yname,
-        value: ""
-      });
+    var symb;
+    for (var s = 0; s < symbols.length; s += 1) {
+      symb = symbols[s];
+      for (var i = 0; i < users[symb].length; i += 1) {
+        buffer += options.fn({
+          name: users[symb][i][0],
+          value: users[symb][i][1]
+        });
+      }
     }
 
     return buffer;
--- app/templates/helpers/user-icon.js
+++ app/templates/helpers/user-icon.js
@@ -3,12 +3,18 @@ define('templates/helpers/user-icon', [
 ], function(Handlebars, _) {
 
   Handlebars.registerHelper("user-icon", function(text) {
-    if (text === "@") {
-      return '<i class="iconic" data-glyph="globe"></i>';
+    if (text === "&") {
+      return '<i class="iconic iconic-sm" data-glyph="globe"></i>';
+    } if (text === "~") {
+      return '<i class="iconic iconic-sm" data-glyph="flag"></i>';
+    } if (text === "@") {
+      return '<i class="iconic iconic-sm" data-glyph="shield"></i>';
+    } if (text === "%") {
+      return '<i class="iconic iconic-sm" data-glyph="contrast"></i>';
     } else if (text === "+") {
-      return '<i class="iconic" data-glyph="bullhorn"></i>';
+      return '<i class="iconic iconic-sm" data-glyph="bullhorn"></i>';
     } else {
-      return '<i class="iconic" data-glyph="person-genderless"></i>';
+      return '<i class="iconic iconic-sm" data-glyph="person-genderless"></i>';
     }
   });
 
