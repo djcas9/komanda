483ffb0963a92aed812709e8c029099a40660825
Dustin Willis Webber <dustin.webber@gmail.com>
2014-06-27 22:47:23 -0400

    format fixes

--- app/client.js
+++ app/client.js
@@ -143,10 +143,10 @@ define([
     }
     var data = message.args.slice(2);
 
-    switch(message.command) {
+    switch (message.command) {
       // away
       case "rpl_away":
-        if(data.length > 1) {
+        if (data.length > 1) {
           self._whois[target].away = data[1];
         } else {
           self._whois[target].away = data[0];
@@ -271,14 +271,11 @@ define([
       $("li.channel-item[data-server-id=\"" + self.options.uuid + "\"][data-name=\"Status\"]").removeClass("offline");
     });
 
-    self.socket.addListener("connection:end", function() {
-    });
+    self.socket.addListener("connection:end", function() {});
 
-    self.socket.addListener("connection:abort", function(max, count) {
-    });
+    self.socket.addListener("connection:abort", function(max, count) {});
 
-    self.socket.addListener("connection:timeout", function() {
-    });
+    self.socket.addListener("connection:timeout", function() {});
 
     self.socket.addListener("connection:error", function(error) {
       var message = "Komanda Error: " + (error.hasOwnProperty("message") ? error.message : "Unknown Error");
@@ -294,8 +291,7 @@ define([
       $("li.channel-item[data-server-id=\"" + self.options.uuid + "\"][data-name=\"Status\"]").addClass("offline");
     });
 
-    self.socket.addListener("connection:reconnect", function(retry) {
-    });
+    self.socket.addListener("connection:reconnect", function(retry) {});
 
     self.socket.addListener("connection:disconnect", function(retry) {
       clearInterval(self.reconnectCheck);
@@ -319,7 +315,11 @@ define([
       if (self.topics.hasOwnProperty(self.options.uuid)) {
         if (self.topics[self.options.uuid].hasOwnProperty(channel)) {
           var topic = self.topics[self.options.uuid][channel];
-          if (topic) channelTopic = topic;
+
+          if (topic) {
+            channelTopic = topic;
+          }
+
         }
       }
 
@@ -397,7 +397,7 @@ define([
 
       self.channelsView.render();
 
-      self.addMessage(channel, nick + " has joined the room. ["+message.prefix+"]");
+      self.addMessage(channel, nick + " has joined the room. [" + message.prefix + "]");
 
       Komanda.vent.trigger("join", data);
     });
@@ -483,7 +483,7 @@ define([
           uuid: uuid.v4()
         };
 
-        if(isNotice) {
+        if (isNotice) {
           var cmd = dd.text.split(" ");
           dd.text = cmd.slice(2).join(" ");
           dd.to = cmd[1];
@@ -500,7 +500,7 @@ define([
           });
         }
 
-        if(!isNotice) {
+        if (!isNotice) {
           self.socket.say(dd.to, dd.text);
         }
 
@@ -510,12 +510,11 @@ define([
       };
 
       if (data.message.trim()[0] === "/") {
-        Komanda.command.handle(self, data, function (client, data, args) {
+        Komanda.command.handle(self, data, function(client, data, args) {
           // default, called when no matches
           client.socket.send.apply(client.socket, data.message.slice(1).split(" "));
         });
-      }
-      else {
+      } else {
         render();
       }
     });
@@ -525,32 +524,35 @@ define([
     // String: 4
     // RegExp: 5
     // Function: 6
-    Komanda.cmd("msg", function (client, data, args) {
-      Komanda.vent.trigger(client.options.uuid + ":send", { target: args[0], message: args.slice(1).join(" ") });
+    Komanda.cmd("msg", function(client, data, args) {
+      Komanda.vent.trigger(client.options.uuid + ":send", {
+        target: args[0],
+        message: args.slice(1).join(" ")
+      });
       Komanda.vent.trigger(client.options.uuid + ":pm", args[0]);
     }, 4);
 
-    Komanda.cmd("query", function (client, data, args) {
+    Komanda.cmd("query", function(client, data, args) {
       Komanda.vent.trigger(client.options.uuid + ":pm", args[0]);
     }, 4);
 
-    Komanda.cmd("me", function (client, data, args) {
+    Komanda.cmd("me", function(client, data, args) {
       client.socket.action(data.target, args.join(" "));
       //client.addMessage(data.target, args.join(" "), true);
     }, 4);
 
-    Komanda.cmd("whois", function (client, data, args) {
+    Komanda.cmd("whois", function(client, data, args) {
       if (args.length > 0) {
         client._whois[args[0].toLowerCase()] = {};
         client.socket.send("WHOIS", args[0]);
       }
     }, 4);
 
-    Komanda.cmd("part", function (client, data, args) {
+    Komanda.cmd("part", function(client, data, args) {
       var channels = (args[0]) ? args[0].split(",") : [data.target];
       var msg = args.slice(1).join(" ") || "Bye!";
 
-      _.each(channels, function (channel) {
+      _.each(channels, function(channel) {
         // TODO: central channel name validation
         if (!channel.match(/^[#&]/)) return;
 
@@ -564,32 +566,29 @@ define([
       });
     }, 4);
 
-    Komanda.cmd("clear", function (client, data, args) {
+    Komanda.cmd("clear", function(client, data, args) {
       $("div.channel[data-server-id=\"" + client.options.uuid + "\"][data-name=\"" + data.target + "\"] div.messages").html("");
     }, 4);
 
-    Komanda.cmd("notice", function (client, data, args) {
+    Komanda.cmd("notice", function(client, data, args) {
       client.socket.notice(args[0], args.slice(1).join(" "));
       //render(true); // is this needed?
     }, 4);
 
-    Komanda.cmd("set", function (client, data, args) {
+    Komanda.cmd("set", function(client, data, args) {
       var setting;
 
       if (args.length === 0) {
         client.addMessage("Status", JSON.stringify(Komanda.settings.toJSON()));
-      }
-      else if (args.length === 1) {
+      } else if (args.length === 1) {
         setting = Komanda.settings.get(args[0]);
         client.addMessage("Status", args[0] + " = " + _.isObject(setting) || _.isArray(setting) ? JSON.stringify(setting) : setting);
-      }
-      else if (args.length === 2) {
+      } else if (args.length === 2) {
         var val;
 
         try {
           val = JSON.parse(args[1]);
-        }
-        catch (e) {
+        } catch (e) {
           val = args[1];
         }
 
@@ -665,15 +664,14 @@ define([
       $("li.channel-item[data-server-id=\"" + self.options.uuid + "\"][data-name=\"Status\"] span.current-nick").html(self.nick);
     });
 
-    self.socket.addListener("pm", function(nick, text, message) {
-    });
+    self.socket.addListener("pm", function(nick, text, message) {});
 
     self.socket.addListener("channellist", function(channel_list) {
       // only do the first hundred until we have a window to handle them all,
       // so we don"t deal with freezing
-      _.each(channel_list.slice(0,100), function(channel) {
+      _.each(channel_list.slice(0, 100), function(channel) {
         var msg = channel.name + " (" + channel.users + ")";
-        if(channel.topic) {
+        if (channel.topic) {
           msg += " || " + channel.topic;
         }
         self.addMessage("Status", msg);
@@ -681,12 +679,12 @@ define([
     });
 
     self.socket.addListener("notice", function(nick, to, text, message) {
-      if ( to.match(/^[#&]/) ) {
+      if (to.match(/^[#&]/)) {
         self.sendMessage(nick, to, text, message, undefined, true);
       } else {
         // PM
 
-        if(!nick) {
+        if (!nick) {
           // this is a server status message
           self.addMessage("Status", message.args.join(" "));
         } else {
@@ -714,7 +712,7 @@ define([
 
     self.socket.addListener("message", function(nick, to, text, message) {
 
-      if ( to.match(/^[#&]/) ) {
+      if (to.match(/^[#&]/)) {
         self.sendMessage(nick, to, text, message);
       } else {
         // PM
@@ -743,24 +741,24 @@ define([
 
     self.socket.addListener("action", function(nick, to, text, message) {
 
-      if ( to.match(/^[#&]/) ) {
+      if (to.match(/^[#&]/)) {
         self.sendMessage(nick, to, text, message, false, false, true);
       } else {
         // PM
 
-         self.buildPM(nick, function() {
-            self.sendMessage(nick, to, text, message, true, false, true);
+        self.buildPM(nick, function() {
+          self.sendMessage(nick, to, text, message, true, false, true);
 
-            if (window.Notification && Komanda.settings.get("notifications.highlight")) {
-              var n = new Notification("Private Message From " + nick, {
-                tag: "Komanda",
-                body: nick + text
-              });
+          if (window.Notification && Komanda.settings.get("notifications.highlight")) {
+            var n = new Notification("Private Message From " + nick, {
+              tag: "Komanda",
+              body: nick + text
+            });
 
-              n.onClick = function() {
-                alert("word");
-              };
-            }
+            n.onClick = function() {
+              alert("word");
+            };
+          }
         });
       }
 
@@ -818,14 +816,18 @@ define([
         self.findChannel(channel).set("modes", modes.trim());
       } else if (message.rawCommand === "401" || message.rawCommand === "403") {
         var chan = self.findChannel(message.args[1]);
-        if(chan) {
+        if (chan) {
           self.removeAndCleanChannel(chan, self.options.uuid);
         }
         var box = Helpers.limp.box(Popup, {
           title: "Error #" + message.rawCommand,
           content: message.args[2] + ": " + message.args[1],
           buttons: [
-            { classes: "right", close: true, name: "OK" }
+            {
+              classes: "right",
+              close: true,
+              name: "OK"
+            }
           ]
         }, {
           onOpen: function() {
@@ -845,9 +847,9 @@ define([
         self.addMessage("Status", message.args.join(" "));
       }
       // Komanda.vent.trigger("raw", {
-        // message: message,
-        // server: self.options.uuid,
-        // name: self.options.name
+      // message: message,
+      // server: self.options.uuid,
+      // name: self.options.name
       // });
     });
 
@@ -891,7 +893,7 @@ define([
 
       self.removeUser(channel, nick);
 
-      self.addMessage(channel, nick + " has left the room. " + (reason ? "["+reason+"]" : "") + "");
+      self.addMessage(channel, nick + " has left the room. " + (reason ? "[" + reason + "]" : "") + "");
 
       Komanda.vent.trigger("part", data);
     });
@@ -922,7 +924,7 @@ define([
       _.each(self.channels.models, function(channel) {
         if (channel.get("names").hasOwnProperty(nick)) {
           self.removeUser(channel.get("channel"), nick);
-          self.addMessage(channel.get("channel"), nick + " has left IRC. " + (reason ? "["+reason+"]" : "") + "");
+          self.addMessage(channel.get("channel"), nick + " has left IRC. " + (reason ? "[" + reason + "]" : "") + "");
         }
       });
 
@@ -1107,7 +1109,7 @@ define([
     }
 
     var html;
-    if(isNotice) {
+    if (isNotice) {
       data.sender = self.me(nick);
       data.toChannel = !!to.match(/^[#&]/);
       html = Notice(data);
@@ -1116,7 +1118,7 @@ define([
       // We check that the message received is not a pm and is not a highlight.
       if (data.flip !== true && data.highlight !== true) {
         Komanda.vent.trigger("komanda:soundnotification", "chat");
-      // If it was not a normal message, we check if it is a pm
+        // If it was not a normal message, we check if it is a pm
       } else if (data.flip === true) {
         Komanda.vent.trigger("komanda:soundnotification", "pm");
       }
@@ -1151,7 +1153,8 @@ define([
           if (data.highlight) {
             Komanda.store[server][(flip ? nick : to)] = 2;
           } else {
-            if (Komanda.store[server][(flip ? nick : to)] != 2) Komanda.store[server][(flip ? nick : to)] = 1;
+            if (Komanda.store[server][(flip ? nick : to)] != 2)
+              Komanda.store[server][(flip ? nick : to)] = 1;
           }
 
         } else {
@@ -1162,7 +1165,8 @@ define([
           if (data.highlight) {
             Komanda.store[server][(flip ? nick : to)] = 2;
           } else {
-            if (Komanda.store[server][(flip ? nick : to)] != 2) Komanda.store[server][(flip ? nick : to)] = 1;
+            if (Komanda.store[server][(flip ? nick : to)] != 2)
+              Komanda.store[server][(flip ? nick : to)] = 1;
           }
         }
 
@@ -1201,7 +1205,8 @@ define([
   Client.prototype.buildPM = function(nick, callback) {
     var self = this;
 
-    if (!nick) nick = "Status";
+    if (!nick)
+      nick = "Status";
 
     var chan = self.findChannel(nick);
 
@@ -1262,3 +1267,4 @@ define([
 
   return Client;
 });
+
